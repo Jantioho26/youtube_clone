ffb8023d700bc0bb85ca686f2901f3a624dd3e60
Joy Antioho <j.wildfong@yahoo.com>
2026-08-24 16:39:10 -0400

    youtube_clone

---
+++ youtube_clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6_2mGUzZmQwtWdBW6owwtnQ3rYl8R7dI",
+  authDomain: "clone-8c94e.firebaseapp.com",
+  projectId: "clone-8c94e",
+  storageBucket: "clone-8c94e.firebasestorage.app",
+  messagingSenderId: "395315217849",
+  appId: "1:395315217849:web:dbbfdad681c0f9b8ae2268"
+};
+
+
